ab3b540913f76c79c3e67283e28139fb13c770c2
arifpatanduk <arifpatanduk2@gmail.com>
2023-08-07 16:01:32 +0700

    chore<post>: filter bad words for title & desc

--- controllers/posts/PostController.js
+++ controllers/posts/PostController.js
@@ -1,6 +1,8 @@
 const expressAsyncHandler = require("express-async-handler")
 const Post = require("../../model/post/Post")
 const validateMongodbId = require("../../utils/validateMongoDBId")
+const BadWordsFilter = require("bad-words")
+const User = require("../../model/user/User")
 
 // create
 const createPostCtrl = expressAsyncHandler(async (req, res) => {
@@ -12,13 +14,25 @@ const createPostCtrl = expressAsyncHandler(async (req, res) => {
     // append user to request body
     req.body.user = _id
 
+    // check for bad words in title and description
+    const filter = new BadWordsFilter()
+    const isTitleProfane = filter.isProfane(req.body.title)
+    const isDescProfane = filter.isProfane(req.body.description)
+    
+    // block user if isProfane
+    if (isTitleProfane || isDescProfane) {
+        await User.findByIdAndUpdate(_id, {
+            isBlocked: true
+        })
+        throw new Error('Creating post failed because its contains profane words. You have been blocked!')
+    }
+
     try {
         const post = await Post.create(req.body)
         res.json(post)
     } catch (error) {
         res.json(error)
     }
-    
 })
 
 
